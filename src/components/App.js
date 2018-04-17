79bd9714ff5128a20bd8f0a02d623361f93f86ef
nackhoon kang <infinitylv@naver.com>
2018-04-18 01:36:12 +0900

    redux-counter

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import Counter from './Counter';
+import CounterContainer from '../containers/CounterContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <CounterContainer></CounterContainer>
+    );
+  }
+}
+
+export default App;
